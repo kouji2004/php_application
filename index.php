712281ff02804cdd2aeacb6b35d4291cd3b7bca9
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-18 15:27:51 +0900

    css

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php
-echo "hello";
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>PHP掲示板</title>
+</head>
+
+<body>
+  <h1>掲示板アプリ</h1><br>
+  <div class="boardWrapper">
+    <form class="fromWrapper">
+      
+    </form>
+  </div>
+</body>
+
+</html>
